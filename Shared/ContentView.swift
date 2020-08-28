fa6803ead9934d4ba2828604c3b7374b75e1dc95
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-08-27 20:51:40 -0700

    simplified

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -25,7 +25,7 @@ struct ContentView_Previews: PreviewProvider {
 
 struct Stroke {
     var points = [CGPoint]()
-    func appendTo(path:inout Path) {
+    func append(to path:inout Path) {
         if self.points.count == 0 {
             return
         }
@@ -36,38 +36,30 @@ struct Stroke {
     }
 }
 
-private let strokes2:[Stroke] = [
-    Stroke(points: [CGPoint(x: 0, y:0), CGPoint(x: 200, y:100)]),
-    Stroke(points: [CGPoint(x: 100, y:200), CGPoint(x: 200, y:100)]),
-];
-
 struct Canvas: View {
     @State private var strokes = [Stroke]()
     @State private var currentStroke = Stroke()
     var body: some View {
         let drag = DragGesture(minimumDistance: 0.1)
             .onChanged({ (value) in
-                print(value.location);
                 self.currentStroke.points.append(value.location)
             })
             .onEnded({ (value) in
-                print(value.location);
                 self.strokes.append(self.currentStroke)
                 self.currentStroke.points.removeAll()
             })
         return GeometryReader { geometry in
             Path { path in
-                for stroke in strokes2 {
-                    stroke.appendTo(path: &path)
-                }
+                path.move(to: .init(x: 0, y: 0))
+                path.addLine(to: .init(x: 200, y: 200))
                 for stroke in self.strokes {
-                    stroke.appendTo(path: &path)
+                    stroke.append(to: &path)
                 }
-                self.currentStroke.appendTo(path: &path)
+                self.currentStroke.append(to: &path)
             }
             .stroke()
-            .gesture(drag)
         }
-        //.frame(maxHeight: .infinity)
+        .frame(maxHeight: .infinity)
+        .gesture(drag)
     }
 }
