a3642551a0c34af1d2f1611c5421c63ef8e728c2
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-08-27 20:15:48 -0700

    Initial Commit

---
+++ Shared/mmCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  mmCanvasApp.swift
+//  Shared
+//
+//  Created by SATOSHI NAKAJIMA on 8/27/20.
+//
+
+import SwiftUI
+
+@main
+struct mmCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
