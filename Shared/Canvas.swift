45022c6e6bf34637b42fe283d0a33bc23c86c867
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-08-28 06:07:10 -0700

    VStack

--- Shared/Canvas.swift
+++ Shared/Canvas.swift
@@ -15,28 +15,34 @@ struct Canvas: View {
 
     var body: some View {
         GeometryReader { geometry in
-            Path { path in
-                for stroke in self.strokes {
-                    stroke.append(to: &path)
+            ZStack {
+                Path { path in
+                    for stroke in self.strokes {
+                        stroke.append(to: &path)
+                    }
                 }
-                self.currentStroke.append(to: &path)
-            }
-            .stroke(self.color, lineWidth: self.lineWidth)
+                .stroke(self.color, lineWidth: self.lineWidth)
                 .background(Color(white: 0.95))
-                .gesture(
-                    DragGesture(minimumDistance: 0.1)
-                        .onChanged({ (value) in
-                            let currentPoint = value.location
-                            if currentPoint.y >= 0
-                                && currentPoint.y < geometry.size.height {
-                                self.currentStroke.points.append(currentPoint)
-                            }
-                        })
-                        .onEnded({ (value) in
-                            self.strokes.append(self.currentStroke)
-                            self.currentStroke = Stroke()
-                        })
-            )
+                Path { path in
+                    self.currentStroke.append(to: &path)
+                }
+                .stroke(self.color, lineWidth: self.lineWidth)
+
+            }
+            .gesture(
+                DragGesture(minimumDistance: 0.1)
+                    .onChanged({ (value) in
+                        let currentPoint = value.location
+                        if currentPoint.y >= 0
+                            && currentPoint.y < geometry.size.height {
+                            self.currentStroke.points.append(currentPoint)
+                        }
+                    })
+                    .onEnded({ (value) in
+                        self.strokes.append(self.currentStroke)
+                        self.currentStroke = Stroke()
+                    })
+        )
         }
         .frame(maxHeight: .infinity)
     }
