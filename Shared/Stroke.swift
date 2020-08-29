e1f7b6b092942e5109ca2884ebfef0cf0e172a7f
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-08-29 05:28:22 -0700

    QuadCurve

--- Shared/Stroke.swift
+++ Shared/Stroke.swift
@@ -13,9 +13,47 @@ struct Stroke {
         if self.points.count == 0 {
             return
         }
-        path.move(to: self.points[0])
-        for point in self.points {
-            path.addLine(to: point)
+        path.move(to: self.points.first!)
+        if (self.points.count <= 2) {
+            path.addLine(to: self.points.last!)
+            return
+        }
+        for i in 1...points.count-2 {
+            let point = points[i]
+            let next = points[i+1]
+            let mid = CGPoint(x: (point.x + next.x)/2, y: (point.y+next.y)/2)
+            path.addQuadCurve(to: mid, control: point)
+        }
+        path.addLine(to: self.points.last!)
+    }
+}
+
+struct Stroke_Previews: PreviewProvider {
+    static let points = [
+        CGPoint(x: 0, y: 0),
+        CGPoint(x: 100, y: 0),
+        CGPoint(x: 0, y: 100),
+        CGPoint(x: 100, y: 100),
+        CGPoint(x: 100, y: 0),
+    ]
+    static var previews: some View {
+        VStack {
+            Path {
+                Stroke(points: Array(points[0..<2])).append(to:&$0)
+            }
+            .stroke()
+            Path {
+                Stroke(points: Array(points[0..<3])).append(to:&$0)
+            }
+            .stroke()
+            Path {
+                Stroke(points: Array(points[0..<4])).append(to:&$0)
+            }
+            .stroke()
+            Path {
+                Stroke(points: points).append(to:&$0)
+            }
+            .stroke()
         }
     }
 }
