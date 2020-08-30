664ff6c0dc1c187309e92a544bdba4ca47b613ad
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-08-29 20:58:38 -0700

    removed Mac

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -11,56 +11,33 @@ struct ContentView: View {
     @State private var canvas = Canvas()
     var body: some View {
         NavigationView {
-            VStack {
-                #if os(iOS)
-                CanvasView(canvas: $canvas)
-                    .navigationBarHidden(true)
-                    .edgesIgnoringSafeArea(.all)
-                    .toolbar {
-                        ToolbarItem(placement: .bottomBar) {
-                            Button(action: { self.canvas.strokes.removeLast() }) {
-                                Text("Undo")
-                            }
-                            .disabled(self.canvas.strokes.count==0)
+            CanvasView(canvas: $canvas)
+                .navigationBarHidden(true)
+                .edgesIgnoringSafeArea(.all)
+                .toolbar {
+                    ToolbarItem(placement: .bottomBar) {
+                        Button(action: { self.canvas.strokes.removeLast() }) {
+                            Text("Undo")
                         }
-                        ToolbarItem(placement: .bottomBar) {
-                            Button(action: { self.canvas.strokes.removeAll() }) {
-                                Image(systemName: "trash")
-                            }
-                            .disabled(self.canvas.strokes.count==0)
-                        }
-                        ToolbarItem(placement: .bottomBar) {
-                            Spacer()
-                        }
-                        ToolbarItem(placement: .bottomBar) {
-                            Button(action: {
-                                self.canvas.markerMode.toggle()
-                            }) {
-                                Image(systemName: self.canvas.markerMode ? "pencil": "pencil.and.outline")
-                            }
-                        }
+                        .disabled(self.canvas.strokes.count==0)
                     }
-                #elseif os(OSX)
-                CanvasView(canvas: $canvas)
-                    .edgesIgnoringSafeArea(.all)
-                HStack {
-                    Button(action: { self.canvas.strokes.removeLast() }) {
-                        Text("Undo")
+                    ToolbarItem(placement: .bottomBar) {
+                        Button(action: { self.canvas.strokes.removeAll() }) {
+                            Image(systemName: "trash")
+                        }
+                        .disabled(self.canvas.strokes.count==0)
                     }
-                    .disabled(self.canvas.strokes.count==0)
-                    Button(action: { self.canvas.strokes.removeAll() }) {
-                        Image(systemName: "trash")
+                    ToolbarItem(placement: .bottomBar) {
+                        Spacer()
                     }
-                    .disabled(self.canvas.strokes.count==0)
-                    Spacer()
-                    Button(action: {
-                        self.canvas.markerMode.toggle()
-                    }) {
-                        Image(systemName: self.canvas.markerMode ? "pencil": "pencil.and.outline")
+                    ToolbarItem(placement: .bottomBar) {
+                        Button(action: {
+                            self.canvas.markerMode.toggle()
+                        }) {
+                            Image(systemName: self.canvas.markerMode ? "pencil": "pencil.and.outline")
+                        }
                     }
                 }
-                #endif
-            }
         }
     }
 }
