dd80e44c2494de2108bcde813ba9d5d702ee0c7b
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-08-31 14:19:15 -0700

    Drag

--- Shared/ImageEmitter.swift
+++ Shared/ImageEmitter.swift
@@ -7,42 +7,10 @@
 
 import SwiftUI
 
-struct AnimatedImage:Hashable {
-    static func == (lhs: AnimatedImage, rhs: AnimatedImage) -> Bool {
-        return lhs.id == rhs.id
-    }
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-    
-    var image:Image
-    var location:CGPoint
-    var complete:()->Void
-    var opacity:CGFloat = 1.0
-    let id = UUID()
-}
-
-struct AnimatedImages {
-    var images = Set<AnimatedImage>()
-    mutating func addElement(at location:CGPoint) {
-        let image = AnimatedImage(image: Image(systemName: "star"), location: location) {
-            // foo
-        }
-        self.images.insert(image)
-    }
-}
-
 struct ImageEmitter: View {
-    @State var images:AnimatedImages
     var body: some View {
         VStack {
-            Text("Foo " + String(images.images.count))
-            /*
-            let array = Array(images.images)
-            ForEach(0..<array.count) { index in
-                Text("Hello ")
-            }
-            */
+            Text("Foo")
         }
     }
 }
@@ -57,14 +25,14 @@ struct ImageEmitter_Previews: PreviewProvider {
 
 struct ImageEmitter_Test: View {
     @State var count = 0
-    @State var images = AnimatedImages()
     var body: some View {
         VStack {
             Text("Hello " + String(count))
-            ImageEmitter(images:images)
+            ImageEmitter()
         }
+        .position()
         .background(Color(.yellow))
-        .gesture(TapGesture().onEnded({ value in
+        .gesture(DragGesture().onChanged({ value in
             self.count = self.count + 1
         }))
     }
